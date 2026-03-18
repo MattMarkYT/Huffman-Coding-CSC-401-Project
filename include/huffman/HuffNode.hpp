7f1702e28595adc016014d96a89c29a1b7e63810
MattMark <matthew99729@gmail.com>
2026-03-17 21:13:54 -0700

    Create Huffman Node struct

---
+++ include/huffman/HuffNode.hpp
@@ -0,0 +1,16 @@
+
+struct HuffNode {
+
+    HuffNode(byte d, int f) : data(d), frequency(f) {}
+    HuffNode(int f, HuffNode* l, HuffNode* r) : frequency(f), left(l), right(r) {}
+
+    byte data;
+    int frequency;
+    HuffNode* left;
+    HuffNode* right;
+
+    bool operator<(const HuffNode& other) const {
+        return frequency > other.frequency;
+    }
+
+};
